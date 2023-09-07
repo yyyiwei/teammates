9d19cff1b2aa9bf630ed88363e893916bf6b5983
yyyiwei <87693878+yyyiwei@users.noreply.github.com>
2023-09-07 23:01:38 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   transform: {
     '^.+\\.ts?$': 'ts-jest'
   },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  clearMocks: true,
   collectCoverageFrom: [
     'src/web/app/**/*.ts',
     '!src/web/app/**/*.module.ts',
