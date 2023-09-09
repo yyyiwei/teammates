acbfa756be4b1b52a334cf766f5855934f62c723
oyster6 <117273291+oyster6@users.noreply.github.com>
2023-09-09 21:57:36 +0800

    Update UserSettings.java

--- src/main/java/teammates/storage/entity/UserSettings.java
+++ src/main/java/teammates/storage/entity/UserSettings.java
@@ -1,6 +1,7 @@
 package teammates.storage.entity;
 
 import java.time.Instant;
+
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
